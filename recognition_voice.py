3cfcb76fb27a5f62a40ba5231f19f6020ecaab99
Jesus Auriol Villamarin Ortiz <jvillamarin@asesoftware.com>
2019-12-24 14:17:25 -0500

    Se agrego acciones como buscar y encontrar ubicación

--- recognition_voice.py
+++ recognition_voice.py
@@ -1,11 +1,14 @@
 import speech_recognition as sr
+from time import ctime
+import webbrowser
 r =  sr.Recognizer()
 
-def voice_recognition(): 
-    print('Initializing Voice Recognition...')
+
+def voice_recognition(ask = False): 
     recognition_result = ''
     with sr.Microphone() as source:
-        print('¿Te puedo ayudar en algo?')
+        if ask: print(ask) 
+        else: print('¿Te puedo ayudar en algo?')
         audio_data = r.listen(source)
         try:
             recognition_result = r.recognize_google(audio_data, language='es-CO')
@@ -16,6 +19,27 @@ def voice_recognition():
 
     return recognition_result
 
+def recognize_action(action_data):
+    action_data = action_data.lower()
+    print(action_data)
+    if 'adiós' in action_data:
+        print('Hasta luego, fue un gusto servirle')
+        exit()
+    if 'qué hora es' in action_data: 
+        print(f'El tiempo actual es: {ctime()}')
+    if 'buscar' in action_data:
+        to_search = voice_recognition('¿Que quisieras buscar?')
+        url = f'https://google.com/search?q={to_search}'
+        webbrowser.get().open(url)
+        print(f'Esto fue lo que encontro para: {to_search}')
+    if 'encontrar ubicación' in action_data:
+        location = voice_recognition('¿Cual es la ubicación?')
+        url = f'https://google.nl/maps/place/{location}/&amp'
+        webbrowser.get().open(url)
+        print(f'Esto fue lo que encontro para: {location}')
+
 if __name__ == "__main__":
+    print('Initializing Voice Recognition...')
     while True:
-        print(voice_recognition())
+        action_data = voice_recognition()
+        recognize_action(action_data)
